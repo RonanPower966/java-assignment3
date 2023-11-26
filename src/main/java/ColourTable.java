aad5a50f688fa4d63a8efe5907f40c9f4019a0f5
Ronan <ronanpower1967@gmail.com>
2023-11-26 17:08:04 +0000

    Added a constructor and colourNum getter to ColourTable.java. Changed the dependencies in pom.xml to use proper junit5 dependencies. Added a test to test the constructor of ColourTable.java.

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,2 +1,11 @@
 public class ColourTable {
+    private final int colourNum;
+
+    public ColourTable(int num) {
+        this.colourNum = num;
+    }
+
+    public int getColourNum() {
+        return this.colourNum;
+    }
 }
--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ColourTableTest {
+
+    ColourTable table = new ColourTable(4);
+    @Test
+    void TestConstructor(){
+        assertEquals(4,table.getColourNum());
+    }
 }
