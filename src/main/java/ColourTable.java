03f6f1c6eb59d9b331d32a670c7caad3174f7687
Ronan <ronanpower1967@gmail.com>
2023-11-29 00:12:56 +0000

    Refactored the code used to check if colour already exists in palette by using a hashmap to keep track of already used colours.

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,7 +1,11 @@
+import java.util.HashMap;
+
 public class ColourTable {
     private final int colourNum;
     byte[][] palette;
     int paletteIndex = 0;
+
+    HashMap<String, Integer> colourHashMap = new HashMap<>();
 
     public ColourTable(int num) {
         if (num < 2 || num > 1024) {
@@ -13,7 +17,7 @@ public class ColourTable {
             if (tempnum % 2 != 0) {
                 throw new InvalidPaletteNumberException("Number must be a power of 2");
             }
-            tempnum = tempnum/2;
+            tempnum = tempnum / 2;
         }
         this.colourNum = num;
         this.palette = new byte[num][3];
@@ -22,20 +26,19 @@ public class ColourTable {
 
     public void add(int r, int g, int b) {
         //If next index added would be out of bounds
-        if(this.paletteIndex>=this.palette.length){
+        if (this.paletteIndex >= this.palette.length) {
             throw new ExceededPaletteMaximumException("Exceeded maximum palette capacity");
         }
 
         //RGB values can only be between 0 and 255
-        if(r>255 || g>255 || b>255 || r<0 || g<0 || b<0) {
+        if (r > 255 || g > 255 || b > 255 || r < 0 || g < 0 || b < 0) {
             throw new InvalidRGBValueException("RGB values must be between 0 and 255 inclusive.");
         }
-        //Checks each colour in the palette and checks if colour already exists
-        for (byte[] bytes : this.palette) {
-            if (bytes[0] == (byte) r && bytes[1] == (byte) g && bytes[2] == (byte) b) {
-                throw new ColourAlreadyExistsException("Colour already exists in palette");
-            }
+        String colour_string = "" + r + g + b;
+        if (this.colourHashMap.containsKey(colour_string)) {
+            throw new ColourAlreadyExistsException("Colour already exists in palette");
         }
+        this.colourHashMap.put(colour_string, 1);
 
         this.palette[this.paletteIndex][0] = (byte) r;
         this.palette[this.paletteIndex][1] = (byte) g;
@@ -43,6 +46,7 @@ public class ColourTable {
         this.paletteIndex = this.paletteIndex + 1;
 
     }
+
     public int getColourNum() {
         return this.colourNum;
     }
